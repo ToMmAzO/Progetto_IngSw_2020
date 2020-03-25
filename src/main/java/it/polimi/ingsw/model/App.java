85c23fb64ef4b2f663141c516fb043dc3237d5e4
RaffaeleSimeoni <raffaele.simeoni@mail.polimi.it>
2020-03-25 16:54:08 +0100

    termine prove git

--- src/main/java/it/polimi/ingsw/model/App.java
+++ src/main/java/it/polimi/ingsw/model/App.java
@@ -9,9 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Bella" );
-        System.out.println( "Bella2" );
-        System.out.println( "Hello World?" );
     }
 
 }
