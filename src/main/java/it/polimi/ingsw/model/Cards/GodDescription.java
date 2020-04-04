879c3bd48e6565e8bb160133c3b765e8b47b0654
LucaQuagliana <10569853@polimi.it>
2020-04-04 20:52:21 +0200

    Delete irrelevant comment

--- src/main/java/it/polimi/ingsw/model/Cards/GodDescription.java
+++ src/main/java/it/polimi/ingsw/model/Cards/GodDescription.java
@@ -18,19 +18,4 @@ public class GodDescription {
 
     }
 
-    /*
-
-    singoli attributi con nome che le specifica
-
-    public static String apolloDescription = "Il tuo lavoratore può spostarsi nella casella di un lavoratore avversario, mandandolo nella tua attuale casella";
-    public static String artemisDescription = "Il tuo lavoratore può spostarsi una volta in più, ma non può tornare nella casella in sui è partito";
-    public static String athenaDescription = "Se nel tuo turno uno dei tuoi lavoratori è salito di livello, in questo turno i lavoratori degli avversari non possono salire di livello";
-    public static String atlasDescription = "Il tuo lavoratore può costruire una cupola su qualsiasi livello, compreso il terreno";
-    public static String demeterDescription = "Il tuo lavoratore può costruire una volta in più, ma non nella stessa casella";
-    public static String hephaestusDescription = "Il tuo lavoratore può costruire un blocco aggiuntuvo (non una cupola) al di sopra del primo blocco";
-    public static String minotaurDescription = "Il tuo lavoratore può spostarsi nella casella di un lavoratore avversario se la casella successiva nella stessa direzione è libera, spostando il lavoratore avversario in questa (indipendentemente dal livello)";
-    public static String panDescription = "Vinci anche se il tuo lavoratore è sceso di due o più livelli";
-    public static String prometheusDescription = "Se il tuo lavoratore non sale di livello, può costruire prima e dopo essere stato mosso";
-     */
-
 }
