828af294b18ac184c98f62b3e4cc220a84fc550c
RaffaeleSimeoni <raffaele.simeoni@mail.polimi.it>
2020-04-04 17:16:57 +0200

    definition of method: getWorkerSelected()

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.model;
 
+import java.util.Scanner;
+
 public class Player {
 
     private String nickname;
@@ -36,8 +38,31 @@ public class Player {
     }
 
     public Worker getWorkerSelected(){
-        //codice
-        return workers[0];//esempio
+        boolean correct;
+        int selection;
+        System.out.println("select the worker you want to use: ");
+        System.out.println("write '1' to use worker number 1 ");
+        System.out.println("write '2' to use worker number 2 ");
+        System.out.println("than press Enter ");
+        do {
+            Scanner scanner = new Scanner(System.in);
+            selection = scanner.nextInt();
+            if (selection == '1' || selection == '2')
+                correct = true;
+            else
+                correct = false;
+
+            switch (selection) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    System.out.println("you can only insert '1' or '2'");
+                    break;
+            }
+        }while(correct == false);
+    return workers[selection-1];
     }
 
 }
