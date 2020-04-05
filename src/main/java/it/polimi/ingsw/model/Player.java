68df69645848dae65947f32bc1e30efb3e30c34f
RaffaeleSimeoni <raffaele.simeoni@mail.polimi.it>
2020-04-05 17:25:14 +0200

    definition of method: chooseColor()

--- src/main/java/it/polimi/ingsw/model/Player.java
+++ src/main/java/it/polimi/ingsw/model/Player.java
@@ -11,12 +11,65 @@ public class Player {
     private Worker[] workers;
     private Divinity godChoice;
 
+    public Player(String nickname){
+        this.nickname = nickname;
+    }
+    public static void main(String[]args){
+
+        Player p1 = new Player("raffo");
+        Player p2 = new Player("raffo2");
+        Player p3 = new Player("raffo3");
+        for(Color color: Color.values())
+            color.init();
+        p1.chooseColor();
+        p2.chooseColor();
+        p3.chooseColor();
+    }
+
     public void setNickname(String nickname) {
         this.nickname = nickname;
     }
 
-    public void setColor(Color color) {
+    //used in chooseColor() to initialize Player's attribute 'color'
+    private void setColor(Color color) {
         this.color = color;
+    }
+
+    //let the user know wich colors are available and make him choose one
+    public void chooseColor(){
+        System.out.println("write 'RED' , 'YELLOW' or 'BLUE' to choose your color");
+        for(Color color: Color.values()){
+            Boolean x = false;
+            x = color.isAvailiable();
+            if(x==true)
+                System.out.println("color:" + color + " is availiable");
+            else
+                System.out.println("color:" + color + " is NOT availiable");
+        }
+
+        System.out.println("what's your choice? : ");
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+        choice = choice.toUpperCase();
+        System.out.println("ho letto: " + choice);
+        switch (choice){
+            case "RED":
+                this.setColor(Color.RED);
+                Color.RED.booked();
+                break;
+            case "YELLOW":
+                this.setColor(Color.YELLOW);
+                Color.YELLOW.booked();
+                break;
+            case "BLUE":
+                this.setColor(Color.BLUE);
+                Color.BLUE.booked();
+                break;
+            default:
+                System.out.println("something's wrong!");
+                break;
+        }
+
     }
 
     public void setGodChoice(Divinity godChoice) {
