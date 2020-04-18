f4e8dfafde141a1931783cf0d32f5699cd123d9b
RaffaeleSimeoni <raffaele.simeoni@mail.polimi.it>
2020-04-18 11:50:13 +0200

    changes in WorkerMinotaur canMove() and implementation of canPush()

--- src/main/java/it/polimi/ingsw/model/Workers/WorkerMinotaur.java
+++ src/main/java/it/polimi/ingsw/model/Workers/WorkerMinotaur.java
@@ -11,9 +11,64 @@ public class WorkerMinotaur extends Worker {
         super(idWorker, coordX, coordY);
     }
 
+    public boolean canPush(int i,int j){
+
+            if (i==this.getCoordX() && j!=this.getCoordY()){         //caso in cui il worker avversario è sulla stessa colonna di Minotauro
+                int temp = this.getCoordY() - j;
+                if(temp > 0){        //il worker avversario si trova a coord minori di Minotauro --> controllo se può spingerlo alla cella i-1
+                    if(ActionManager.validCoords(i-1,j) && Map.noWorkerHere(i-1,j) && Map.getCellBlockType(i-1,j) != BlockType.CUPOLA)
+                        return true;
+                }
+                if(temp < 0) {        //il worker avversario si trova a coord maggiori di Minotauro --> controllo se può spingerlo alla cella i+1
+                    if (ActionManager.validCoords(i + 1, j) && Map.noWorkerHere(i + 1, j) && (Map.getCellBlockType(i + 1, j) != BlockType.CUPOLA))
+                        return true;
+                }
+            }
+
+            if (i!=this.getCoordX() && j==this.getCoordY()){         //caso in cui il worker avversario è sulla stessa riga di Minotauro
+                int temp = this.getCoordX() - i;
+                if(temp > 0){        //il worker avversario si trova a coord minori di Minotauro --> controllo se può spingerlo alla cella j-1
+                    if(ActionManager.validCoords(i,j-1) && Map.noWorkerHere(i,j-1) && Map.getCellBlockType(i,j-1) != BlockType.CUPOLA)
+                        return true;
+                }
+                if(temp < 0) {        //il worker avversario si trova a coord maggiori di Minotauro --> controllo se può spingerlo alla cella j+1
+                    if (ActionManager.validCoords(i, j+1) && Map.noWorkerHere(i, j+1) && Map.getCellBlockType(i , j+1) != BlockType.CUPOLA)
+                        return true;
+                }
+            }
+            return false;
+    }
+
+
+
+
     @Override
     public boolean canMove() {
-        int m = getCoordX() - 2;
+        for (int j = this.getCoordY() - 1; j <= this.getCoordY() + 1; j++) {
+            for (int i = this.getCoordX() - 1; i <= this.getCoordX() + 1; i++) {
+                if (ActionManager.validCoords(i, j)) {
+                    if (TurnManager.cannotGoUp()){
+                        if ((Map.getCellBlockType(i, j).getAbbreviation() <= this.getCoordZ()) && (Map.getCellBlockType(i, j) != BlockType.CUPOLA))
+                            if (Map.noWorkerHere(i, j))
+                                return true;
+                            if (!Map.noWorkerHere(i, j))          //controllo separato per verificare i casi in cui può spostare il worker avversario
+                                if (this.canPush(i, j))
+                                    return true;
+                    }
+                    if (!TurnManager.cannotGoUp()) {
+                        if ((Map.getCellBlockType(i, j).getAbbreviation() <= this.getCoordZ() + 1) && (Map.getCellBlockType(i, j) != BlockType.CUPOLA))
+                            if (Map.noWorkerHere(i, j))
+                                return true;
+                            if (!Map.noWorkerHere(i, j))          //controllo separato per verificare i casi in cui può spostare il worker avversario
+                                if (this.canPush(i, j))
+                                    return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+        /* int m = getCoordX() - 2;
         int n = getCoordY() - 2;
 
         for (int i = getCoordX() - 1; i <= getCoordX() + 1; i++) {           //se no funziona --> while
@@ -37,8 +92,8 @@ public class WorkerMinotaur extends Worker {
             }
             m = m + 2;
         }
-        return false;
-    }
+        return false;*/
+
 
     @Override
     public void changePosition(int newX, int newY){
