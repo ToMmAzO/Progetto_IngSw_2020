7cfe522b4b679a158e7df8c05a9660c7a9514314
LucaQuagliana <10569853@polimi.it>
2020-05-13 16:53:18 +0200

    Implementation of ActionManagerTest

--- src/test/java/it/polimi/ingsw/controller/ActionManagerTest.java
+++ src/test/java/it/polimi/ingsw/controller/ActionManagerTest.java
@@ -1,5 +1,9 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.model.board.BlockType;
+import it.polimi.ingsw.model.board.Map;
+import it.polimi.ingsw.model.cards.God;
+import it.polimi.ingsw.model.workers.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -43,8 +47,86 @@ class ActionManagerTest {
     }
 
     @Test
-    void verifyCoordinateMovementTest(){
+    void verifyCoordinateMovement_IrregularCoordinatesTest(){
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 5, 5));
+    }
 
+    @Test
+    void verifyCoordinateMovement_NotAroundCoordinatesTest(){
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 3, 3));
+    }
+
+    @Test
+    void verifyCoordinateMovement_ActualPositionTest(){
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 0, 0));
+    }
+
+    @Test
+    void verifyCoordinateMovement_OccupiedCellTest(){
+        new WorkerDemeter("W1", 1, 1);
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_OccupiedCellApolloTest1(){//tuo lavoratore
+        new WorkerApollo("RED1", 1, 1);
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerApollo("RED2", 0, 0), God.APOLLO, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_CupolaTest(){
+        Map.getInstance().setCellBlockType(1, 1, BlockType.CUPOLA);
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_PowerAthenaTest1(){//senza salire
+        TurnManager.getInstance().setAllowHeight(false);
+        assertTrue(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_PowerAthenaTest2(){//salire ma sei Athena
+        TurnManager.getInstance().setAllowHeight(false);
+        Map.getInstance().setCellBlockType(1, 1, BlockType.BLOCK1);
+        assertTrue(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAthena("W", 0, 0), God.ATHENA, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_PowerAthenaTest3(){//salire e non sei athena
+        TurnManager.getInstance().setAllowHeight(false);
+        Map.getInstance().setCellBlockType(1, 1, BlockType.BLOCK1);
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_PowerPrometheusTest1(){//senza salire
+        TurnManager.getInstance().setAllowHeightPrometheus(false);
+        assertTrue(ActionManager.getInstance().verifyCoordinateMovement(new WorkerPrometheus("W", 0, 0), God.PROMETHEUS, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_PowerPrometheusTest2(){//salire
+        TurnManager.getInstance().setAllowHeightPrometheus(false);
+        Map.getInstance().setCellBlockType(1, 1, BlockType.BLOCK1);
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerPrometheus("W", 0, 0), God.PROMETHEUS, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_NoGoUpTest(){
+        assertTrue(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_SingleGoUpTest(){
+        Map.getInstance().setCellBlockType(1, 1, BlockType.BLOCK1);
+        assertTrue(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 1, 1));
+    }
+
+    @Test
+    void verifyCoordinateMovement_DoubleGoUpTest(){
+        Map.getInstance().setCellBlockType(1, 1, BlockType.BLOCK2);
+        assertFalse(ActionManager.getInstance().verifyCoordinateMovement(new WorkerAtlas("W", 0, 0), God.ATLAS, 1, 1));
     }
 
     @Test
