4975193e6c180e0ea48a61e86dfcab7a9435fdc8
LucaQuagliana <10569853@polimi.it>
2020-05-17 22:52:38 +0200

    Implementation of TurnManagerTest

--- src/test/java/it/polimi/ingsw/controller/TurnManagerTest.java
+++ src/test/java/it/polimi/ingsw/controller/TurnManagerTest.java
@@ -286,6 +286,7 @@ class TurnManagerTest {
         GameManager.getInstance().setCurrPlayer(p);
         TurnManager.getInstance().workerChoice(1);
 
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(1, 0));
         try {
             TurnManager.getInstance().construction(1, 0);
         } catch(IndexOutOfBoundsException ignore){}
@@ -294,8 +295,25 @@ class TurnManagerTest {
     }
 
 
-    //CHRONUS
+    //CHRONUS win
 
+
+    @Test
+    void construction_ChronusNotWinTest() {
+        Player p = new Player("Player");
+        p.setGodChoice(God.CHRONUS);
+        p.setWorker1(0, 0);
+        p.setWorker2(0, 1);
+        GameManager.getInstance().setCurrPlayer(p);
+        TurnManager.getInstance().workerChoice(1);
+
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(1, 0));
+        try {
+            TurnManager.getInstance().construction(1, 0);
+        } catch(IndexOutOfBoundsException ignore){}
+        assertEquals(BlockType.BLOCK1, Map.getInstance().getCellBlockType(1, 0));
+        assertEquals(GameState.WAIT_TURN, Game.getInstance().getGameState(p));
+    }
 
     @Test
     void construction_DemeterCanRebuildTest() {
@@ -306,6 +324,7 @@ class TurnManagerTest {
         GameManager.getInstance().setCurrPlayer(p);
         TurnManager.getInstance().workerChoice(1);
 
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(1, 0));
         TurnManager.getInstance().construction(1, 0);
         assertEquals(BlockType.BLOCK1, Map.getInstance().getCellBlockType(1, 0));
         assertEquals(GameState.QUESTION_DEMETER, Game.getInstance().getGameState(p));
@@ -321,6 +340,7 @@ class TurnManagerTest {
         TurnManager.getInstance().workerChoice(1);
         Map.getInstance().setCellBlockType(1, 1, BlockType.CUPOLA);
 
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(1, 0));
         try {
             TurnManager.getInstance().construction(1, 0);
         } catch(IndexOutOfBoundsException ignore){}
@@ -337,6 +357,7 @@ class TurnManagerTest {
         GameManager.getInstance().setCurrPlayer(p);
         TurnManager.getInstance().workerChoice(1);
 
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(1, 0));
         TurnManager.getInstance().construction(1, 0);
         assertEquals(BlockType.BLOCK1, Map.getInstance().getCellBlockType(1, 0));
         assertEquals(GameState.QUESTION_HESTIA, Game.getInstance().getGameState(p));
@@ -359,15 +380,105 @@ class TurnManagerTest {
         assertEquals(GameState.WAIT_TURN, Game.getInstance().getGameState(p));
     }
 
+    @Test
+    void secondConstructionDemeter_Test() {
+        Player p = new Player("Player");
+        p.setGodChoice(God.DEMETER);
+        p.setWorker1(0, 0);
+        p.setWorker2(0, 1);
+        GameManager.getInstance().setCurrPlayer(p);
+        TurnManager.getInstance().workerChoice(1);
+        TurnManager.getInstance().construction(1, 0);
+
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(1, 1));
+        try {
+            TurnManager.getInstance().secondConstructionDemeter(1, 1);
+        } catch(IndexOutOfBoundsException ignore){}
+        assertEquals(BlockType.BLOCK1, Map.getInstance().getCellBlockType(1, 1));
+        assertEquals(GameState.WAIT_TURN, Game.getInstance().getGameState(p));
+    }
 
 
+    @Test
+    void secondConstructionDemeter_InSamePositionTest() {
+        Player p = new Player("Player");
+        p.setGodChoice(God.DEMETER);
+        p.setWorker1(0, 0);
+        p.setWorker2(0, 1);
+        GameManager.getInstance().setCurrPlayer(p);
+        TurnManager.getInstance().workerChoice(1);
+        TurnManager.getInstance().construction(1, 0);
 
+        assertEquals(BlockType.BLOCK1, Map.getInstance().getCellBlockType(1, 0));
+        TurnManager.getInstance().secondConstructionDemeter(1, 0);
+        assertEquals(BlockType.BLOCK1, Map.getInstance().getCellBlockType(1, 0));
+    }
 
+    @Test
+    void secondConstructionHestia_Test() {
+        Player p = new Player("Player");
+        p.setGodChoice(God.HESTIA);
+        p.setWorker1(0, 0);
+        p.setWorker2(0, 1);
+        GameManager.getInstance().setCurrPlayer(p);
+        TurnManager.getInstance().workerChoice(1);
+        TurnManager.getInstance().construction(1, 1);
 
+        assertEquals(BlockType.BLOCK1, Map.getInstance().getCellBlockType(1, 1));
+        try {
+            TurnManager.getInstance().secondConstructionHestia(1, 1);
+        } catch(IndexOutOfBoundsException ignore){}
+        assertEquals(BlockType.BLOCK2, Map.getInstance().getCellBlockType(1, 1));
+        assertEquals(GameState.WAIT_TURN, Game.getInstance().getGameState(p));
+    }
 
+    @Test
+    void secondConstructionHestia_InPerimeterTest() {
+        Player p = new Player("Player");
+        p.setGodChoice(God.HESTIA);
+        p.setWorker1(0, 0);
+        p.setWorker2(0, 1);
+        GameManager.getInstance().setCurrPlayer(p);
+        TurnManager.getInstance().workerChoice(1);
+        TurnManager.getInstance().construction(1, 1);
 
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(0, 0));
+        TurnManager.getInstance().secondConstructionHestia(0, 0);
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(0, 0));
+    }
 
+    @Test
+    void constructionCupola_Test() {
+        Player p = new Player("Player");
+        p.setGodChoice(God.ATLAS);
+        p.setWorker1(0, 0);
+        p.setWorker2(0, 1);
+        GameManager.getInstance().setCurrPlayer(p);
+        TurnManager.getInstance().workerChoice(1);
 
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(1, 0));
+        try{
+            TurnManager.getInstance().constructionCupola(1, 0);
+        } catch(IndexOutOfBoundsException ignore){}
+        assertEquals(BlockType.CUPOLA, Map.getInstance().getCellBlockType(1, 0));
+        assertEquals(GameState.WAIT_TURN, Game.getInstance().getGameState(p));
+    }
 
+    @Test
+    void doubleConstruction_Test() {
+        Player p = new Player("Player");
+        p.setGodChoice(God.HEPHAESTUS);
+        p.setWorker1(0, 0);
+        p.setWorker2(0, 1);
+        GameManager.getInstance().setCurrPlayer(p);
+        TurnManager.getInstance().workerChoice(1);
+
+        assertEquals(BlockType.GROUND, Map.getInstance().getCellBlockType(1, 0));
+        try {
+            TurnManager.getInstance().doubleConstruction(1, 0);
+        } catch(IndexOutOfBoundsException ignore){}
+        assertEquals(BlockType.BLOCK2, Map.getInstance().getCellBlockType(1, 0));
+        assertEquals(GameState.WAIT_TURN, Game.getInstance().getGameState(p));
+    }
 
 }
