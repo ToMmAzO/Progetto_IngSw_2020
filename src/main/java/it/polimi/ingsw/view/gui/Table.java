6ee6f5967649155cfd4ec01c2415d5e37531dd52
ToMmAzO <tommaso4.pozzi@mail.polimi.it>
2020-05-25 19:04:13 +0200

    First steps on GUI

--- src/main/java/it/polimi/ingsw/view/gui/Table.java
+++ src/main/java/it/polimi/ingsw/view/gui/Table.java
@@ -1,25 +1,31 @@
 package it.polimi.ingsw.view.gui;
 
+import it.polimi.ingsw.model.board.Map;
+import it.polimi.ingsw.model.workers.Worker;
+import it.polimi.ingsw.model.workers.WorkerDemeter;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.swing.SwingUtilities.isLeftMouseButton;
+
 public class Table {
 
     private final JFrame gameFrame;
     private final BoardPanel boardPanel;
-    private final WorkerPanel workerPanel;
 
     private final static String backGroundPath = "src/main/java/it/polimi/ingsw/view/gui/img/SantoriniBoard.png";
-    //private final static String godCardPath = "src/main/java/it/polimi/ingsw/view/gui/img/cards/";
-    //godCardPath + nome divinità + ".png"
-
+    private final static String iconsPath = "src/main/java/it/polimi/ingsw/view/gui/img/icons/";
 
     private final static Dimension TABLE_DIMENSION = new Dimension(1440,810);
     private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(600,600);
@@ -41,11 +47,19 @@ public class Table {
 
         layeredPane.add(getBackGround(), Integer.valueOf(0));
 
+        new Map();
+
+        try {
+            new WorkerDemeter("RED1", 2, 3);
+        }catch(NullPointerException ignore){}
+
+        try {
+            new WorkerDemeter("YEL1", 4, 3);
+        }catch(NullPointerException ignore){}
+
+
         boardPanel = new BoardPanel();
         layeredPane.add(boardPanel, Integer.valueOf(1));
-
-        workerPanel = new WorkerPanel();
-        layeredPane.add(workerPanel, Integer.valueOf(2));
 
         //gameFrame.pack();     la finestra assuma le dimensioni minime necessarie e sufficienti affinchè ciò che contiene sia visualizzato secondo le sue dimensioni ottimali
         gameFrame.add(layeredPane);
@@ -67,33 +81,12 @@ public class Table {
         return label;
     }
 
-    private class WorkerPanel extends JPanel{
-        WorkerPanel(){
-            super(new GridLayout(5,5));
-            for(int i = 0; i < 25; i++) {
-                JButton pulsante = new JButton(String.valueOf(i));
-                pulsante.setContentAreaFilled(false);
-                pulsante.setBorderPainted(false);
-                int finalI = i;
-                pulsante.addActionListener(e -> System.out.println(finalI));
-                add(pulsante);
-            }
-            //setBorder(new EmptyBorder(100, 100, 100, 100));
-            setBackground(new Color(0, 0, 0, 0));       //Sfondo invisibile
-            setOpaque(false);                                      //Vedi sotto
-            setBounds(420, 105, 600, 600);
-            setSize(BOARD_PANEL_DIMENSION);
-            //setVisible(false);
-            validate();
-        }
-    }
-
     private class BoardPanel extends JPanel{
         final List<TilePanel> boardTiles;
 
-        BoardPanel(){
+        BoardPanel() throws IOException {
             super(new GridLayout(5,5));
-            this.boardTiles = new ArrayList<>();
+            boardTiles = new ArrayList<>();
             for(int i = 0; i < NUM_TILES; i++) {
                 final TilePanel tilePanel = new TilePanel(this, i);
                 boardTiles.add(tilePanel);          //lista
@@ -111,14 +104,163 @@ public class Table {
 
     private class TilePanel extends JPanel{     //piastrella
         private final int tileId;
+        private int coordX, coordY;
 
-        TilePanel(final BoardPanel boardPanel, final int tileId){
+        TilePanel(final BoardPanel boardPanel, final int tileId) throws IOException {
             super(new GridBagLayout());
             this.tileId = tileId;
+            tileIdInCoordinate();
             //setPreferredSize(TILE_PANEL_DIMENSION);
             assignTileColor();
+            assignTilePieceIcon(Map.getInstance());
+
+            addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(final MouseEvent e) {
+                    if(isLeftMouseButton(e)){
+
+                    }
+                }
+
+                @Override
+                public void mousePressed(final MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseReleased(final MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseEntered(final MouseEvent e) {
+
+                }
+
+                @Override
+                public void mouseExited(final MouseEvent e) {
+
+                }
+            });
+
             //setBackground(new Color(0, 0, 0, 0));
             validate();
+        }
+
+        private void tileIdInCoordinate(){
+            switch (tileId){
+                case 0 -> {
+                    coordX = 0;
+                    coordY = 0;
+                }
+                case 1 -> {
+                    coordX = 0;
+                    coordY = 1;
+                }
+                case 2 -> {
+                    coordX = 0;
+                    coordY = 2;
+                }
+                case 3 -> {
+                    coordX = 0;
+                    coordY = 3;
+                }
+                case 4 -> {
+                    coordX = 0;
+                    coordY = 4;
+                }
+                case 5 -> {
+                    coordX = 1;
+                    coordY = 0;
+                }
+                case 6 -> {
+                    coordX = 1;
+                    coordY = 1;
+                }
+                case 7 -> {
+                    coordX = 1;
+                    coordY = 2;
+                }
+                case 8 -> {
+                    coordX = 1;
+                    coordY = 3;
+                }
+                case 9 -> {
+                    coordX = 1;
+                    coordY = 4;
+                }
+                case 10 -> {
+                    coordX = 2;
+                    coordY = 0;
+                }
+                case 11 -> {
+                    coordX = 2;
+                    coordY = 1;
+                }
+                case 12 -> {
+                    coordX = 2;
+                    coordY = 2;
+                }
+                case 13 -> {
+                    coordX = 2;
+                    coordY = 3;
+                }
+                case 14 -> {
+                    coordX = 2;
+                    coordY = 4;
+                }
+                case 15 -> {
+                    coordX = 3;
+                    coordY = 0;
+                }
+                case 16 -> {
+                    coordX = 3;
+                    coordY = 1;
+                }
+                case 17 -> {
+                    coordX = 3;
+                    coordY = 2;
+                }
+                case 18 -> {
+                    coordX = 3;
+                    coordY = 3;
+                }
+                case 19 -> {
+                    coordX = 3;
+                    coordY = 4;
+                }
+                case 20 -> {
+                    coordX = 4;
+                    coordY = 0;
+                }
+                case 21 -> {
+                    coordX = 4;
+                    coordY = 1;
+                }
+                case 22 -> {
+                    coordX = 4;
+                    coordY = 2;
+                }
+                case 23 -> {
+                    coordX = 4;
+                    coordY = 3;
+                }
+                case 24 -> {
+                    coordX = 4;
+                    coordY = 4;
+                }
+            }
+        }
+
+        private void assignTilePieceIcon(final Map map) throws IOException {
+            BufferedImage image;
+            this.removeAll();
+            if(map.noWorkerHere(coordX, coordY)) {
+                image = ImageIO.read(new File(iconsPath + map.getCellBlockType(coordX, coordY).toString() + ".png"));
+            }else {
+                image = ImageIO.read(new File(iconsPath + map.getCellBlockType(coordX, coordY).toString() + map.getWorkerInCell(coordX, coordY).getIdWorker().substring(0, 3) + ".png"));
+            }
+            add(new JLabel(new ImageIcon(image)));
         }
 
         private void assignTileColor() {
