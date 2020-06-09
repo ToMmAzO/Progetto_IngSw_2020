03f53bf3064d871c7c92b155fb857f393216e317
ToMmAzO <tommaso4.pozzi@mail.polimi.it>
2020-06-09 21:49:07 +0200

    GUI: Upgrade Table

--- src/main/java/it/polimi/ingsw/view/gui/Table.java
+++ src/main/java/it/polimi/ingsw/view/gui/Table.java
@@ -7,6 +7,7 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -18,15 +19,17 @@ public class Table extends JPanel{
     private final static String iconsPath = "src/main/java/it/polimi/ingsw/view/gui/img/icons/";
 
     private final static Dimension TABLE_DIMENSION = new Dimension(1280,720);
+    private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(530,530);
+
     private final Image image = ImageIO.read(new File(backGroundPath));
 
-    private final Cell[] cells = new Cell[25];
+    private final BoardPanel boardPanel;
 
     public Table() throws IOException {
         super();
         this.setSize(TABLE_DIMENSION);
         setLayout(null);
-        addCells();
+        add(boardPanel = new BoardPanel());
     }
 
     @Override
@@ -35,43 +38,52 @@ public class Table extends JPanel{
         g.drawImage(image, 0, 0, null);
     }
 
-    public void addCells() throws IOException {
-        int x = 0, y = 0;
-        int xPos = 375, yPos = 95;
-        for(int i = 0; i < 25; i++) {
-            cells[i] = new Cell(x, y);
-            add(cells[i]);
-            cells[i].setLocation(xPos, yPos);
-            y++;
-            xPos = xPos + 106;
-            if(y == 5){
-                y = 0;
-                xPos = 375;
-                x++;
-                yPos = yPos + 106;
+    public void updateMap() throws IOException {
+        boardPanel.update();
+    }
+
+    private static class BoardPanel extends JPanel{
+
+        private final TilePanel[] tiles = new TilePanel[25];
+
+        public BoardPanel() throws IOException {
+            super(new GridLayout(5,5));
+            int x = 0;
+            int y = 0;
+            for(int i = 0; i < 25; i++) {
+                tiles[i] = new TilePanel(x, y);
+                add(tiles[i]);
+                y++;
+                if(y == 5){
+                    y = 0;
+                    x++;
+                }
+            }
+            setBackground(new Color(0, 0, 0, 0));
+            setLocation(375, 95);
+            setSize(BOARD_PANEL_DIMENSION);
+            setOpaque(false);
+        }
+
+        public void update() throws IOException {
+            for(TilePanel t: tiles){
+                t.assignTilePieceIcon();
             }
         }
     }
 
-    public void updateMap() throws IOException {
-        for(Cell c: cells){
-            c.assignIcon();
-        }
-    }
-
-    private static class Cell extends JButton{
+    private static class TilePanel extends JButton{
 
         private final int coordX, coordY;
 
-        public Cell(int coordX, int coordY) throws IOException {
-            setSize(106, 106);
-            this.coordX = coordX;
-            this.coordY = coordY;
-            assignIcon();
+        public TilePanel(int x, int y) throws IOException {
+            coordX = x;
+            coordY = y;
+            assignTilePieceIcon();
             setBorderPainted(false);
             setBackground(new Color(0, 0, 0, 0));
-            addMouseListener(new MouseListener(){
 
+            addMouseListener(new MouseListener() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     if(isLeftMouseButton(e)){
@@ -83,7 +95,7 @@ public class Table extends JPanel{
                                 }
                             }
                         } else {
-                            String coordinate = coordX + "," + coordY;
+                            String coordinate = coordX + ", " + coordY;
                             System.out.println(coordinate);
                             Gui.getInstance().asyncWriteToSocket(coordinate);
                         }
@@ -91,37 +103,35 @@ public class Table extends JPanel{
                 }
 
                 @Override
-                public void mousePressed(MouseEvent e){}
+                public void mousePressed(MouseEvent e){ }
 
                 @Override
-                public void mouseReleased(MouseEvent e){}
+                public void mouseReleased(MouseEvent e){ }
 
                 @Override
-                public void mouseEntered(MouseEvent e){}
+                public void mouseEntered(MouseEvent e){ }
 
                 @Override
-                public void mouseExited(MouseEvent e){}
-
+                public void mouseExited(MouseEvent e){ }
             });
-            setVisible(true);
+
             setOpaque(false);
         }
 
-        private void assignIcon() throws IOException {
+        private void assignTilePieceIcon() throws IOException {
             Image image;
             if(Gui.getInstance().getMap().noWorkerHere(coordX, coordY)) {
                 if(!Gui.getInstance().getMap().getCellBlockType(coordX, coordY).equals(BlockType.GROUND)) {
                     image = ImageIO.read(new File(iconsPath + Gui.getInstance().getMap().getCellBlockType(coordX, coordY).toString() + ".png"));
-                } else{
+                }else{
                     setIcon(null);
                     return;
                 }
-            } else{
+            }else {
                 image = ImageIO.read(new File(iconsPath + Gui.getInstance().getMap().getCellBlockType(coordX, coordY).toString() + Gui.getInstance().getMap().getWorkerInCell(coordX, coordY).getIdWorker().substring(0, 3) + ".png"));
             }
             setIcon(new ImageIcon(image));
         }
-
     }
 
 }
